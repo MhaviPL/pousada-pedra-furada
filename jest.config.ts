e4d6ae616c0250b926d07ce8c430794f6eed5004
MhaviPL <matheuslp330@gmail.com>
2026-02-27 19:15:39 -0300

    feat: configuações no package

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,16 @@
-/** @type {import('jest').Config} */
-const config = {
- testEnvironment: "jest-environment-jsdom",
- setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
- moduleNameMapper: {
- "\\.(css|less|scss|sass)$": "identity-obj-proxy",
- },
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  // Caminho para o seu app Next.js para carregar o next.config.js e arquivos .env
+  dir: './',
+})
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 };
-module.exports = config;
